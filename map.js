2e828a77f5d9f752fc60a2d77cb04b262187e049
Gangascoob <greg.scobie@gmail.com>
2022-04-05 22:55:19 +0100

    Adding world map and json file

---
+++ map.js
@@ -0,0 +1,32 @@
+var projection = d3.geoMercator() 
+				   .center([ -50, 65 ]) 
+				   .translate([ 0, 0 ]) 
+				   .scale([350]); 
+
+			//Define path generator
+			var path = d3.geoPath()
+					.projection(projection);
+
+
+
+
+
+
+
+var svg = d3.select("#mapcontainer")
+	.append("svg")
+	.attr("width", "100%")
+	.attr("height", "90%");
+
+d3.json("worldmap.json").then(function(json){
+
+    svg.selectAll("path")
+	   .data(json.features)
+	   .enter()
+	   .append("path")
+	   .attr("d", path)
+	   .attr("stroke", "rgba(8, 81, 156, 0.2)")
+           .attr("fill", "rgba(8, 81, 156, 0.6)");
+           
+	   
+})
