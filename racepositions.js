f84a53f9a78097076f10de8e9b05515ebfa14553
Gangascoob <greg.scobie@gmail.com>
2022-04-06 23:02:36 +0100

    Update racepositions.js

--- racepositions.js
+++ racepositions.js
@@ -11,7 +11,7 @@ sampledata.push({id: data.raceId, driver: data.driverId, lap: data.lap, position
 }).then(function filter(){
 filteredracedata = sampledata.filter(function(d){return d.id == raceidselect});
 
-console.log(filteredracedata[1]);
+console.log(filteredracedata[0]);
 
 racedata = [{id: filteredracedata.raceId, driver: filteredracedata.driverId, lap: filteredracedata.lap, position: filteredracedata.position}];
 
