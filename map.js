2b29dcc76590844abbb37219618738866dc31b51
Gangascoob <greg.scobie@gmail.com>
2022-04-06 13:11:07 +0100

    Update map.js

--- map.js
+++ map.js
@@ -37,8 +37,8 @@ d3.csv("data/circuits2021.csv").then(function(csv){
 		.data(csv)
 		.enter()
 		.append("circle")
-		.attr("cx", function (d) { return projection([d.lng, d.lat]);})
-		.attr("cy", function (d) {return projection([d.lat, d.lng]);})
+		.attr("cx", function (d) { return projection([+d.lng, +d.lat]);})
+		.attr("cy", function (d) {return projection([+d.lng, +d.lat]);})
 		.attr("r", "8px")
 		.attr("fill", "red")
 
