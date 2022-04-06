8ccef91254b4c7297fb751744c5a1281a0ff5173
Gangascoob <greg.scobie@gmail.com>
2022-04-06 21:32:02 +0100

    Testing data filtering for race positions

---
+++ racepositions.js
@@ -0,0 +1,16 @@
+let sampledata = [];
+let filteredracedata = [];
+let racedata = [];
+var raceidselect;
+
+raceidselect = 9;
+
+d3.csv("data/laptimes2021.csv", function(csv){
+sampledata.push({id: csv.raceId, driver: csv.driverId, lap: csv.lap, position: csv.position});
+}).then(function filter(){
+filteredracedata = sampledata.filter(function(d){return d.raceId == raceidselect});
+
+
+})
+
+console.log(filteredracedata[5]);
