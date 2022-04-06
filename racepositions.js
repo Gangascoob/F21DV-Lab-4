3a82396bc0629503ebdf1f556068cf30cbd7df7e
Gangascoob <greg.scobie@gmail.com>
2022-04-06 22:51:18 +0100

    Update racepositions.js

--- racepositions.js
+++ racepositions.js
@@ -5,15 +5,15 @@ var raceidselect;
 
 raceidselect = 9;
 
-d3.csv("data/laptimes2021.csv", function(csv){
-sampledata.push({id: csv.raceId, driver: csv.driverId, lap: csv.lap, position: csv.position});
+d3.csv("data/laptimes2021.csv", function(data){
+sampledata.push({id: data.raceId, driver: data.driverId, lap: data.lap, position: data.position});
+console.log(sampledata[1]);
 }).then(function filter(){
 filteredracedata = sampledata.filter(function(d){return d.raceId == raceidselect});
 
 console.log(filteredracedata);
 
 racedata = [{id: filteredracedata.raceId, driver: filteredracedata.driverId, lap: filteredracedata.lap, position: filteredracedata.position}];
-console.log(racedata[0]);
+
 });
 
-console.log(racedata[5]);
