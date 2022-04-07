ab4e00b5a3c2cec938b06def0124334d14cecf34
Gangascoob <greg.scobie@gmail.com>
2022-04-07 23:10:58 +0100

    Adding all checkboxes

--- drivers.js
+++ drivers.js
@@ -40,3 +40,84 @@ if (checkbox[0].checked){
 else d3.selectAll("#mercedes").transition().duration(500).attr("opacity", 0);
 
 }
+
+function RedBullCheck(){
+    var checkbox = document.getElementsByName("redbull");
+    if (checkbox[0].checked){
+        d3.selectAll("#redbull").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#redbull").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function FerrariCheck(){
+    var checkbox = document.getElementsByName("ferrari");
+    if (checkbox[0].checked){
+        d3.selectAll("#ferrari").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#ferrari").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function McLarenCheck(){
+    var checkbox = document.getElementsByName("mclaren");
+    if (checkbox[0].checked){
+        d3.selectAll("#mclaren").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#mclaren").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function AlpineCheck(){
+    var checkbox = document.getElementsByName("alpine");
+    if (checkbox[0].checked){
+        d3.selectAll("#alpine").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#alpine").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function AlphaTauriCheck(){
+    var checkbox = document.getElementsByName("alphatauri");
+    if (checkbox[0].checked){
+        d3.selectAll("#alphatauri").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#alphatauri").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function AlfaRomeoCheck(){
+    var checkbox = document.getElementsByName("alfaromeo");
+    if (checkbox[0].checked){
+        d3.selectAll("#alfaromeo").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#alfaromeo").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function WilliamsCheck(){
+    var checkbox = document.getElementsByName("williams");
+    if (checkbox[0].checked){
+        d3.selectAll("#williams").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#williams").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function HaasCheck(){
+    var checkbox = document.getElementsByName("haas");
+    if (checkbox[0].checked){
+        d3.selectAll("#haas").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#haas").transition().duration(500).attr("opacity", 0);
+    
+}
+
+function AstonMartinCheck(){
+    var checkbox = document.getElementsByName("astonmartin");
+    if (checkbox[0].checked){
+        d3.selectAll("#astonmartin").transition().duration(500).attr("opacity", 1);
+    } 
+    else d3.selectAll("#astonmartin").transition().duration(500).attr("opacity", 0);
+    
+}
