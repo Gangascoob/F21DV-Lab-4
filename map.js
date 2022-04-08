63ca84014180795de9d3a8f5ee79936183e32e69
Gangascoob <greg.scobie@gmail.com>
2022-04-08 01:56:04 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,9 +1,12 @@
 
+
+//initialising variables + arrays
 var circuitmarks = []
-var circuitidorder = [3, 21, 75, 4, 6, 73, 34, 70, 70, 9, 11, 13, 39, 14, 71, 5, 69, 32, 18, 78, 77, 24]
+var circuitidorder = [3, 21, 75, 4, 6, 73, 34, 70, 70, 9, 11, 13, 39, 14, 71, 5, 69, 32, 18, 78, 77, 24];
 var raceidorder = [1052, 1053, 1054, 1055, 1056, 1057, 1059, 1058, 1060, 1061, 1062, 1063, 1064, 1065, 1066, 1067, 1069, 1070, 1071, 1051, 1072, 1073];
 var circuitposition = 0;
 
+//defines projection so that it's centered well
 var projection = d3.geoEquirectangular() 
 				   .center([ -30, 0 ]) 
 				   .translate([ 350, 220 ]) 
@@ -14,7 +17,7 @@ var path = d3.geoPath()
 			 .projection(projection);
 
 
-
+//reads and pushes circuit co-ordinates to array
 d3.csv("data/circuits2021.csv", function(data){
 
 	circuitmarks.push({lng: data.lng, lat: data.lat, circuitid: data.circuitId});
