ad9100c2fe79e18de68b784a45df88ca4fd64cbf
Gangascoob <greg.scobie@gmail.com>
2022-04-08 02:16:43 +0100

    Update racepositions.js

--- racepositions.js
+++ racepositions.js
@@ -6,15 +6,17 @@ var raceidselect;
 
 raceidselect = 1052;
 
-
+//filters out the current selected race data.
 function racefilter(){
 
     let sampledata = [];
 
     d3.csv("data/laptimes2021.csv", function(data){
+        //pushing the wanted information into a temporary array
         sampledata.push({id: data.raceId, driver: data.driverId, lap: data.lap, position: data.position});
 
         }).then(function filter(){
+            //filtering out the race data by raceid so we only get the currently selected race.
             filteredracedata = sampledata.filter(function(d){return d.id == raceidselect});
             racedata = [];
             //pushing into new array so that is visible outside function
@@ -25,7 +27,7 @@ function racefilter(){
 };
 
 
-
+//calls the filtering function and then calls the updategraph function after a short delay so that data has time to process.
 function loadnext(){
 
     racefilter();
@@ -37,7 +39,7 @@ function loadnext(){
 
 };
 
-
+//initial calls to load data on pageload
 racefilter();
 setTimeout(function(){
     updategraph(racedata);
